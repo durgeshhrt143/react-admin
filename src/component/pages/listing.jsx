af661b52a8b1f1ab449adcee0d3bd74f5bc614ac
Durgesh Singh <singh.durgesh5@gmail.com>
2019-06-14 16:45:42 +0530

    ui changes for firm listing page

--- src/component/pages/listing.jsx
+++ src/component/pages/listing.jsx
@@ -13,238 +13,171 @@ const Listing = props => {
       </section>
       <ul className="list-unstyled listingPage">
         <li className="col-md-3 listCon">
-          <span className="listConBg">
-            <img src={bg} alt="background" className="img-responsive" />
-          </span>
-          <figure className="listFigure">
-            <img className="img-responsive" src={google} alt="google" />
-          </figure>
-          <div className="ProfileCard-content text-center">
-            <div>
-              <Link className="fullname" to="/">
-                Aeon 1
+          <div className="profile">
+            <Link to="/" className="setting">
+              <em>
+                <i className="fa fa-cog" aria-hidden="true" />
+              </em>
+            </Link>
+            <figure className="listFigure">
+              <img className="img-responsive" src={google} alt="google" />
+            </figure>
+            <strong>Durgesh</strong>
+          </div>
+          <div className="postBlock">
+            <div className="postBlockSeperator">
+              <span>10</span>
+              <span>Users</span>
+              <Link to="/">
+                <em>
+                  <i className="fa fa-plus" aria-hidden="true" />
+                </em>
               </Link>
             </div>
-            <span className="ProfileCard">
-              <div className="userBlock">
-                <div>
-                  <strong className="count">
-                    <em>10</em>
-                    <Link to="/">
-                      <em>
-                        <i className="fa fa-plus-circle" aria-hidden="true" />
-                      </em>
-                    </Link>
-                  </strong>
-                  <span className="listFund">
-                    <i className="fa fa-users" aria-hidden="true" />
-                    Users
-                  </span>
-                </div>
-                <div>
-                  <strong className="count">
-                    <em>10</em>
-                    <Link to="/">
-                      <em>
-                        <i className="fa fa-plus-circle" aria-hidden="true" />
-                      </em>
-                    </Link>
-                  </strong>
-                  <span className="listFund">
-                    <i className="fa fa-align-left" aria-hidden="true" />
-                    Funds
-                  </span>
-                </div>
-              </div>
-            </span>
-          </div>
-          <div className="follow">
-            <Link to="/">
-              <i className="fa fa-pencil-square" aria-hidden="true" />
-            </Link>
-            <Link to="/">
-              <i className="fa fa-trash-o" aria-hidden="true" />
-            </Link>
-            <Link to="/">
-              <i className="fa fa-key" aria-hidden="true" />
-            </Link>
+            <div className="postBlockSeperator">
+              <span>10</span>
+              <span>Posts</span>
+              <Link to="/">
+                <em>
+                  <i className="fa fa-plus" aria-hidden="true" />
+                </em>
+              </Link>
+            </div>
           </div>
         </li>
         <li className="col-md-3 listCon">
-          <span className="listConBg">
-            <img src={bg} alt="background" className="img-responsive" />
-          </span>
-          <figure className="listFigure">
-            <img className="img-responsive" src={google} alt="google" />
-          </figure>
-          <div className="ProfileCard-content text-center">
-            <div>
-              <Link className="fullname" to="/">
-                Aeon 1
+          <div className="profile">
+            <Link to="/" className="setting">
+              <em>
+                <i className="fa fa-cog" aria-hidden="true" />
+              </em>
+            </Link>
+            <figure className="listFigure">
+              <img className="img-responsive" src={google} alt="google" />
+            </figure>
+            <strong>Durgesh</strong>
+          </div>
+          <div className="postBlock">
+            <div className="postBlockSeperator">
+              <span>10</span>
+              <span>Users</span>
+              <Link to="/">
+                <em>
+                  <i className="fa fa-plus" aria-hidden="true" />
+                </em>
               </Link>
             </div>
-            <span className="ProfileCard">
-              <div className="userBlock">
-                <div>
-                  <strong className="count">
-                    <em>10</em>
-                    <Link to="/">
-                      <em>
-                        <i className="fa fa-plus-circle" aria-hidden="true" />
-                      </em>
-                    </Link>
-                  </strong>
-                  <span className="listFund">
-                    <i className="fa fa-users" aria-hidden="true" />
-                    Users
-                  </span>
-                </div>
-                <div>
-                  <strong className="count">
-                    <em>10</em>
-                    <Link to="/">
-                      <em>
-                        <i className="fa fa-plus-circle" aria-hidden="true" />
-                      </em>
-                    </Link>
-                  </strong>
-                  <span className="listFund">
-                    <i className="fa fa-align-left" aria-hidden="true" />
-                    Funds
-                  </span>
-                </div>
-              </div>
-            </span>
-          </div>
-          <div className="follow">
-            <Link to="/">
-              <i className="fa fa-pencil-square" aria-hidden="true" />
-            </Link>
-            <Link to="/">
-              <i className="fa fa-trash-o" aria-hidden="true" />
-            </Link>
-            <Link to="/">
-              <i className="fa fa-key" aria-hidden="true" />
-            </Link>
+            <div className="postBlockSeperator">
+              <span>10</span>
+              <span>Posts</span>
+              <Link to="/">
+                <em>
+                  <i className="fa fa-plus" aria-hidden="true" />
+                </em>
+              </Link>
+            </div>
           </div>
         </li>
         <li className="col-md-3 listCon">
-          <span className="listConBg">
-            <img src={bg} alt="background" className="img-responsive" />
-          </span>
-
-          <figure className="listFigure">
-            <img className="img-responsive" src={google} alt="google" />
-          </figure>
-          <div className="ProfileCard-content text-center">
-            <div>
-              <Link className="fullname" to="/">
-                Aeon 1
+          <div className="profile">
+            <Link to="/" className="setting">
+              <em>
+                <i className="fa fa-cog" aria-hidden="true" />
+              </em>
+            </Link>
+            <figure className="listFigure">
+              <img className="img-responsive" src={google} alt="google" />
+            </figure>
+            <strong>Durgesh</strong>
+          </div>
+          <div className="postBlock">
+            <div className="postBlockSeperator">
+              <span>10</span>
+              <span>Users</span>
+              <Link to="/">
+                <em>
+                  <i className="fa fa-plus" aria-hidden="true" />
+                </em>
               </Link>
             </div>
-            <span className="ProfileCard">
-              <div className="userBlock">
-                <div>
-                  <strong className="count">
-                    <em>10</em>
-                    <Link to="/">
-                      <em>
-                        <i className="fa fa-plus-circle" aria-hidden="true" />
-                      </em>
-                    </Link>
-                  </strong>
-                  <span className="listFund">
-                    <i className="fa fa-users" aria-hidden="true" />
-                    Users
-                  </span>
-                </div>
-                <div>
-                  <strong className="count">
-                    <em>10</em>
-                    <Link to="/">
-                      <em>
-                        <i className="fa fa-plus-circle" aria-hidden="true" />
-                      </em>
-                    </Link>
-                  </strong>
-                  <span className="listFund">
-                    <i className="fa fa-align-left" aria-hidden="true" />
-                    Funds
-                  </span>
-                </div>
-              </div>
-            </span>
-          </div>
-          <div className="follow">
-            <Link to="/">
-              <i className="fa fa-pencil-square" aria-hidden="true" />
-            </Link>
-            <Link to="/">
-              <i className="fa fa-trash-o" aria-hidden="true" />
-            </Link>
-            <Link to="/">
-              <i className="fa fa-key" aria-hidden="true" />
-            </Link>
+            <div className="postBlockSeperator">
+              <span>10</span>
+              <span>Posts</span>
+              <Link to="/">
+                <em>
+                  <i className="fa fa-plus" aria-hidden="true" />
+                </em>
+              </Link>
+            </div>
           </div>
         </li>
         <li className="col-md-3 listCon">
-          <span className="listConBg">
-            <img src={bg} alt="background" className="img-responsive" />
-          </span>
-          <figure className="listFigure">
-            <img className="img-responsive" src={google} alt="google" />
-          </figure>
-          <div className="ProfileCard-content text-center">
-            <div>
-              <Link className="fullname" to="/">
-                Aeon 1
+          <div className="profile">
+            <Link to="/" className="setting">
+              <em>
+                <i className="fa fa-cog" aria-hidden="true" />
+              </em>
+            </Link>
+            <figure className="listFigure">
+              <img className="img-responsive" src={google} alt="google" />
+            </figure>
+            <strong>Durgesh</strong>
+          </div>
+          <div className="postBlock">
+            <div className="postBlockSeperator">
+              <span>10</span>
+              <span>Users</span>
+              <Link to="/">
+                <em>
+                  <i className="fa fa-plus" aria-hidden="true" />
+                </em>
               </Link>
             </div>
-            <span className="ProfileCard">
-              <div className="userBlock">
-                <div>
-                  <strong className="count">
-                    <em>10</em>
-                    <Link to="/">
-                      <em>
-                        <i className="fa fa-plus-circle" aria-hidden="true" />
-                      </em>
-                    </Link>
-                  </strong>
-                  <span className="listFund">
-                    <i className="fa fa-users" aria-hidden="true" />
-                    Users
-                  </span>
-                </div>
-                <div>
-                  <strong className="count">
-                    <em>10</em>
-                    <Link to="/">
-                      <em>
-                        <i className="fa fa-plus-circle" aria-hidden="true" />
-                      </em>
-                    </Link>
-                  </strong>
-                  <span className="listFund">
-                    <i className="fa fa-align-left" aria-hidden="true" />
-                    Funds
-                  </span>
-                </div>
-              </div>
-            </span>
-          </div>
-          <div className="follow">
-            <Link to="/">
-              <i className="fa fa-pencil-square" aria-hidden="true" />
-            </Link>
-            <Link to="/">
-              <i className="fa fa-trash-o" aria-hidden="true" />
-            </Link>
-            <Link to="/">
-              <i className="fa fa-key" aria-hidden="true" />
-            </Link>
+            <div className="postBlockSeperator">
+              <span>10</span>
+              <span>Posts</span>
+              <Link to="/">
+                <em>
+                  <i className="fa fa-plus" aria-hidden="true" />
+                </em>
+              </Link>
+            </div>
           </div>
         </li>
+        <li className="col-md-3 listCon">
+          <div className="profile">
+            <Link to="/" className="setting">
+              <em>
+                <i className="fa fa-cog" aria-hidden="true" />
+              </em>
+            </Link>
+            <figure className="listFigure">
+              <img className="img-responsive" src={google} alt="google" />
+            </figure>
+            <strong>Durgesh</strong>
+          </div>
+          <div className="postBlock">
+            <div className="postBlockSeperator">
+              <span>10</span>
+              <span>Users</span>
+              <Link to="/">
+                <em>
+                  <i className="fa fa-plus" aria-hidden="true" />
+                </em>
+              </Link>
+            </div>
+            <div className="postBlockSeperator">
+              <span>10</span>
+              <span>Posts</span>
+              <Link to="/">
+                <em>
+                  <i className="fa fa-plus" aria-hidden="true" />
+                </em>
+              </Link>
+            </div>
+          </div>
+        </li>
+      
       </ul>
     </Aux>
   );
